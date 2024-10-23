2b5250e12809727b5bcee1891a08b1210b6b66ff
ajiths <ajiths@agilecyber.com>
2024-10-23 11:37:07 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'CalculatorScreen.view.dart';
+
+void main() {
+  runApp(ScientificCalculatorApp());
+}
+
+class ScientificCalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ScientificCalculator(),
+    );
+  }
+}
